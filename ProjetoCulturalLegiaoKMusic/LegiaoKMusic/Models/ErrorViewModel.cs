c6f2f2bba1036326168829d7a14e10fe81923713
andre.guterres <andre.lepesteur@gmail.com>
2023-09-26 12:25:44 -0300

    Projeto MVC adicionado

---
+++ ProjetoCulturalLegiaoKMusic/LegiaoKMusic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LegiaoKMusic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
